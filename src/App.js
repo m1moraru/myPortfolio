f360b585d566141e1d2ad02db47ed08dbd8d6656
m1moraru <164013436+m1moraru@users.noreply.github.com>
2025-03-12 19:07:19 +0000

    Fixed routing and added 404.html for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Profile />} />
